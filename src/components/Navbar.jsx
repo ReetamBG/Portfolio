9a70fd37faa1a467187a14fa2ea948a54eae12d6
ReetamBG <r33t4m@gmail.com>
2025-02-18 22:12:03 +0530

    lazy push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,48 @@
 function Navbar() {
     return (
-      <nav className="navbar navbar-expand-lg fixed-top" 
-          style={{ 
-              background: "rgba(0, 0, 0, 0.2)", 
-              backdropFilter: "blur(10px)", 
-              borderBottom: "1px solid rgba(255, 255, 255, 0.2)"
-          }}
-      >
-          <div className="container-fluid px-5">
-              {/* Brand */}
-              <a className="navbar-brand text-white fw-bold fs-4" href="#">Portfolio</a>
-  
-              {/* Toggler Button */}
-              <button 
-                  className="navbar-toggler border-white" 
-                  type="button" 
-                  data-bs-toggle="collapse" 
-                  data-bs-target="#navbarNav" 
-                  aria-controls="navbarNav" 
-                  aria-expanded="false" 
-                  aria-label="Toggle navigation"
-                  style={{ outline: "none", boxShadow: "none" }}
-              >
-                  <span className="navbar-toggler-icon"></span>
-              </button>
-  
-              {/* Navbar Items */}
-              <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                  <ul className="navbar-nav gap-3" style={{ fontSize: "1.1rem" }}>
-                      <li className="nav-item"><a className="nav-link text-secondary" href="#hero">Home</a></li>
-                      <li className="nav-item"><a className="nav-link text-secondary" href="#about-me">About Me</a></li>
-                      <li className="nav-item"><a className="nav-link text-secondary" href="#projects">Projects</a></li>
-                      <li className="nav-item"><a className="nav-link text-secondary" href="#">Contact</a></li>
-                      <li className="nav-item">
-                          {/* <a className="nav-link text-white" href="#">
+        <nav className="navbar navbar-expand-md fixed-top"
+            style={{
+                background: "rgba(0, 0, 0, 0.2)",
+                backdropFilter: "blur(10px)",
+                borderBottom: "1px solid rgba(255, 255, 255, 0.2)"
+            }}
+        >
+            <div className="container-fluid px-4 px-md-5">
+                {/* Brand */}
+                <a className="navbar-brand text-white fw-bold fs-4" href="#">Portfolio</a>
+
+                {/* Toggler Button */}
+                <button
+                    data-bs-theme="dark"
+                    className="navbar-toggler border-secondary"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    style={{ outline: "none", boxShadow: "none" }}
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                {/* Navbar Items */}
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav gap-3" style={{ fontSize: "1.1rem" }}>
+                        <li className="nav-item"><a className="nav-link text-secondary" href="#hero">Home</a></li>
+                        <li className="nav-item"><a className="nav-link text-secondary" href="#about-me">About Me</a></li>
+                        <li className="nav-item"><a className="nav-link text-secondary" href="#projects">Projects</a></li>
+                        <li className="nav-item"><a className="nav-link text-secondary" href="#">Contact</a></li>
+                        <li className="nav-item">
+                            {/* <a className="nav-link text-white" href="#">
                               <svg className="bi" width="24" height="24" fill="white"><use xlinkHref="#cart"></use></svg>
                           </a> */}
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </nav>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
-  }
-  
-  export default Navbar;
-  
+}
+
+export default Navbar;
